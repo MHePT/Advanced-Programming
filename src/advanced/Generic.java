aa12d6b943a2b892dd6b055c14f12acab1950779
henawy <henawy@Dodos-Laptop>
2023-04-05 20:43:10 +0200

    Readability increasing

--- src/advanced/Generic.java
+++ src/advanced/Generic.java
@@ -19,14 +19,18 @@ public class Generic<T extends Object & Serializable & Comparable> {
     }
 
     public static <Data_Type> void swaparray(Data_Type[] arr, int index1, int index2) {
+        
         Data_Type temp;
+        
         temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
 
     public static <Data_Type extends Comparable<Data_Type>> Data_Type maximum(Data_Type x, Data_Type y, Data_Type z) {
+        
         Data_Type max = x;
+        
         if (y.compareTo(max) > 0) 
             max = y;
         
